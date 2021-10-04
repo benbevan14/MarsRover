dd9896ffd198d8c2bbe18f18d0af6ee1fcea2096
Luke Monaghan <L.Monaghan@ucas.ac.uk>
2021-10-04 19:38:19 +0100

    made class for user time

---
+++ MarsRover/UserTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    class UserTime
+    {
+        public static DateTime GetUserTime()
+        {
+            DateTime userTime = DateTime.Now;
+            return (userTime);
+        }
+    }
+}
