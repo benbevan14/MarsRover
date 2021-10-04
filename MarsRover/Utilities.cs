9863eb37cac3d48542312f0d04a25806299f0fd9
Josh Batt <J.Batt@ucas.ac.uk>
2021-10-04 15:23:04 +0100

    Added class to convert dates

---
+++ MarsRover/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    public class Utilities
+    {
+        public static int MarsDayInSeconds = 88775;
+        public static int EarthDayInSeconds = 86400;
+        public decimal DayConversion = MarsDayInSeconds / EarthDayInSeconds;
+
+        public int EarthDateToSol(DateTime earthDate)
+        {
+            int timeDifferenceInDays = (int)earthDate.Subtract(new DateTime(1873, 12, 29)).TotalDays;
+            int MarsSolDate = (int)(timeDifferenceInDays * DayConversion);
+
+            return MarsSolDate;
+        }
+    }
+}
