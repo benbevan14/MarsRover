412632b8e1cdf356a466a89ec71fc289bc22d463
Benjamin Bevan <bbevan196@gmail.com>
2021-10-04 14:00:44 +0100

    added MarsRoverPhotosAPI class

---
+++ MarsRover/MarsRoverPhotosAPI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace MarsRover
+{
+    public class MarsRoverPhotosAPI
+    {
+        public string ApiKey { get; set; }
+        public string BaseUrl { get; set; }
+        public RestClient RestClient { get; set; }
+
+        public MarsRoverPhotosAPI()
+        {
+            ApiKey = ConfigurationManager.AppSettings["apiKey"];
+            BaseUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/";
+            RestClient = new RestClient(BaseUrl);
+        }
+    }
+}
