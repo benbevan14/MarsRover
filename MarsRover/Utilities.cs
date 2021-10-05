e13a9da0e795afeada33ebd45c888ac7bd16a609
Benjamin Bevan <bbevan196@gmail.com>
2021-10-05 14:51:36 +0100

    fixed conversion

--- MarsRover/Utilities.cs
+++ MarsRover/Utilities.cs
@@ -7,24 +7,27 @@ namespace MarsRover
 {
     public class Utilities
     {
-        public static int MarsDayInSeconds = 88775;
-        public static int EarthDayInSeconds = 86400;
-        public decimal DayConversion = MarsDayInSeconds / EarthDayInSeconds;
-
-        public int EarthDateToSol(DateTime earthDate)
+        public static double GetJulianDateUt(double milliseconds)
         {
-            int timeDifferenceInDays = (int)earthDate.Subtract(new DateTime(1873, 12, 29)).TotalDays;
-            int MarsSolDate = (int)(timeDifferenceInDays * DayConversion);
-
-            return MarsSolDate;
+            return 2440587.5 + milliseconds / 8.64e7;
         }
 
-        public DateTime SolToEarthDate(int MarsSolDate)
+        public static double GetJulianDateTt(double julianDateUT)
         {
-            int timeDifferenceInDays = (int)(MarsSolDate / DayConversion);
-            DateTime earthDate = new DateTime(1873, 12, 29).AddDays(timeDifferenceInDays);
-  
-            return earthDate;
+            return julianDateUT + (37 + 32.184) / 86400;
+        }
+
+        public static double GetJ2000(double jdTt)
+        {
+            return jdTt - 2451545.0;
+        }
+
+        public static double GetMarsSolDate()
+        {
+            double jdUt = GetJulianDateUt(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+            double jdTt = GetJulianDateTt(jdUt);
+            double j2000 = GetJ2000(jdTt);
+            return (j2000 - 4.5) / 1.027491252 + 44796.0 - 0.00096;
         }
     }
 }
