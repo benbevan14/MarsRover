dc9a53aed9d7bfe94cac3860e8bf2ebac57f218f
Rhys <r.stedman@ucas.ac.uk>
2021-10-11 12:05:13 +0100

    olaf review code

--- MarsRover/wwwroot/js/site.js
+++ MarsRover/wwwroot/js/site.js
@@ -120,7 +120,19 @@ function draw() {
 var mouseX;
 var mouseY;
 
-canvas.addEventListener("mousemove", checkPos);
+var canvas = document.getElementById("myMenu");
+var context = canvas.getContext("2d");
+var width = canvas.getAttribute('width');
+var height = canvas.getAttribute('height');
+
+//Menu Items
+var bgImage = new Image();
+var logoImage = new Image();
+var playImage = new Image();
+var instructImage = new Image();
+var settingsImage = new Image();
+var creditsImage = new Image();
+var shipImage = new Image();
 
 function checkPos(mouseEvent) {
     mouseX = mouseEvent.pageX - this.offsetLeft;
@@ -156,6 +168,25 @@ var shipY = [0, 0];
 var shipWidth = 35;
 var shipHeight = 40;
 
+//draws images on top
+bgImage.onload = function () {
+    context.drawImage(bgImage, 0, 0);
+};
+logoImage.onload = function () {
+    context.drawImage(logoImage, 50, -10);
+}
+playImage.onload = function () {
+    context.drawImage(playImage, buttonX[0], buttonY[0]);
+}
+instructImage.onload = function () {
+    context.drawImage(instructImage, buttonX[1], buttonY[1]);
+}
+settingsImage.onload = function () {
+    context.drawImage(settingsImage, buttonX[2], buttonY[2]);
+}
+creditsImage.onload = function () {
+    context.drawImage(creditsImage, buttonX[3], buttonY[3]);
+}
 var shipVisible = false;
 var shipSize = shipWidth;
 var shipRotate = 0;
@@ -164,6 +195,130 @@ var shipRotate = 0;
 var fadeId = 0;
 canvas.addEventListener("mouseup", checkClick);
 
+//animation
+var frames = 30;
+var timerId = 0;
+
+timerId = setInterval(update, 1000 / frames);
+
+//update function
+function update() {
+    clear();
+    move();
+    draw();
+}
+// clearing functin 
+function clear() {
+    context.clearRect(0, 0, width, height);
+}
+// variables
+var backgroundY = 0;
+var speed = 1;
+
+//background scrolling
+function move() {
+    backgroundY -= speed;
+    if (backgroundY === -1 * height) {
+        backgroundY = 0;
+    }
+    if (shipSize === shipWidth) {
+        shipRotate = -1;
+    }
+    if (shipSize === 0) {
+        shipRotate = 1;
+    }
+    shipSize += shipRotate;
+}
+// draws the images
+function draw() {
+    context.drawImage(bgImage, 0, backgroundY);
+    context.drawImage(logoImage, 50, -10);
+    context.drawImage(playImage, buttonX[0], buttonY[0]);
+    context.drawImage(instructImage, buttonX[1], buttonY[1]);
+    context.drawImage(settingsImage, buttonX[2], buttonY[2]);
+    context.drawImage(creditsImage, buttonX[3], buttonY[3]);
+    if (shipVisible == true) {
+        context.drawImage(shipImage, shipX[0] - (shipSize / 2), shipY[0], shipSize, shipHeight);
+        context.drawImage(shipImage, shipX[1] - (shipSize / 2), shipY[1], shipSize, shipHeight);
+    }
+}
+
+//check mouse pos
+var mouseX;
+var mouseY;
+
+canvas.addEventListener("mousemove", checkPos);
+
+function checkPos(mouseEvent) {
+    mouseX = mouseEvent.pageX - this.offsetLeft;
+    mouseY = mouseEvent.pageY - this.offsetTop;
+
+    for (i = 0; i < buttonX.length; i++) {
+        if (mouseX > buttonX[i] && mouseX < buttonX[i] + buttonWidth[i]) {
+            if (mouseY > buttonY[i] && mouseY < buttonY[i] + buttonHeight[i]) {
+                shipVisible = true;
+                shipX[0] = buttonX[i] - (shipWidth / 2) - 2;
+                shipY[0] = buttonY[i] + 2;
+                shipX[1] = buttonX[i] + buttonWidth[i] + (shipWidth / 2);
+                shipY[1] = buttonY[i] + 2;
+            }
+        } else {
+            shipVisible = false;
+        }
+    }
+}
+
+if (mouseEvent.pageX || mouseEvent.pageY == 0) {
+    mouseX = mouseEvent.pageX - this.offsetLeft;
+    mouseY = mouseEvent.pageY - this.offsetTop;
+} else if (mouseEvent.offsetX || mouseEvent.offsetY == 0) {
+    mouseX = mouseEvent.offsetX;
+    mouseY = mouseEvent.offsetY;
+}
+//draw the ship
+var shipX = [0, 0];
+var shipY = [0, 0];
+var shipWidth = 35;
+var shipHeight = 40;
+
+var shipVisible = false;
+var shipSize = shipWidth;
+var shipRotate = 0;
+
+//checking mouse clicks
+var fadeId = 0;
+canvas.addEventListener("mouseup", checkClick);
+
+function checkClick(mouseEvent) {
+    for (i = 0; i < buttonX.length; i++) {
+        if (mouseX > buttonX[i] && mouseX < buttonX[i] + buttonWidth[i]) {
+            if (mouseY > buttonY[i] && mouseY < buttonY[i] + buttonHeight[i]) {
+
+            }
+        }
+    }
+}
+
+fadeId = setInterval("fadeOut()", 1000 / frames);
+clearInterval(timerId);
+canvas.removeEventListener("mousemove", checkPos);
+canvas.removeEventListener("mouseup", checkClick);
+var time = 0.0;
+
+function fadeOut() {
+    context.fillStyle = "rgba(0,0,0, 0.2)";
+    context.fillRect(0, 0, width, height);
+    time += 0.1;
+    if (time >= 2) {
+        clearInterval(fadeId);
+        time = 0;
+        timerId = setInterval("update()", 1000 / frames);
+        canvas.addEventListener("mousemove", checkPos);
+        canvas.addEventListener("mouseup", checkClick);
+    }
+}
+
+//end of menu
 function checkClick(mouseEvent) {
     for (i = 0; i < buttonX.length; i++) {
         if (mouseX > buttonX[i] && mouseX < buttonX[i] + buttonWidth[i]) {
@@ -413,6 +568,16 @@ function start_game() {
 }
 
 function restart_game() {
+    if (has_game_ended() === true) {
+        Debug.log("dead");
+        main();
+    }
+}
+
+function highestScoreInput() {
+    
+}
+
     if (has_game_ended() === true) {
         startingGame();
         update();
